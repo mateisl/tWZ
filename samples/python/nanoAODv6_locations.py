6c976545c4ccfa54bac973e65ad9172bcf5cd33d
rosmarie schoefbeck <rosmarie.x@me.com>
2020-09-04 10:16:44 +0200

    samplefile

---
+++ samples/python/nanoAODv6_locations.py
@@ -0,0 +1,2 @@
+mc_2016               = "/mnt/hephy/cms/rosmarie.schoefbeck/tWZ/nanoTuples/tWZ_nAODv6/2016/trilep"
+#data_2016             = "/mnt/hephy/cms/robert.schoefbeck/tWZ/nanoTuples/tWZ_nAODv4_v2/2016/trilep"

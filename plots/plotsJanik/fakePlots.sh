9c5ad027b2489fe3e5df4fc570b0c616be0bf3d3
janik.andrejkovic <janik.andrejkovic@clip-c1-7.cbe.vbc.ac.at>
2020-09-07 20:23:15 +0200

    adding plot scripts for fake estimation

---
+++ plots/plotsJanik/fakePlots.sh
@@ -0,0 +1,2 @@
+python fakePlots.py --plot_directory fake_shape_comparison --logLevel DEBUG |& tee debugFull.out
+#python fakePlots.py --plot_directory fake_shape_comparison --small --logLevel DEBUG |& tee debug.out

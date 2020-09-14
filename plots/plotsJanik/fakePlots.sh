3ab955aacb48478fd1397f88a00a86363d1ed968
janik.andrejkovic <janik.andrejkovic@clip-c2-34.cbe.vbc.ac.at>
2020-09-14 14:30:44 +0200

    moved fake plots in dedicated directory

--- plots/plotsJanik/fakePlots.sh
+++
@@ -1,2 +0,0 @@
-python fakePlots.py --plot_directory fake_shape_comparison --logLevel DEBUG |& tee debugFull.out
-#python fakePlots.py --plot_directory fake_shape_comparison --small --logLevel DEBUG |& tee debug.out
